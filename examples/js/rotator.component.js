ba4125f80da8f0c1c6e27b5ef5369fc6e2dd0fda
EnriqueVega1995 <vegacampos1995@gmail.com>
2022-01-20 17:02:55 -0600

    Added spectator character and toggle physics

--- examples/js/rotator.component.js
+++ examples/js/rotator.component.js
@@ -8,8 +8,8 @@ AFRAME.registerComponent('rotator', {
         console.log("is working");
         
     },
-    tick: function () {
-        //this.el.object3D.rotation.y = Math.sin(((time*this.data.speed)/1000)*Math.PI);
+    tick: function (time,dt) {
+        this.el.object3D.rotation.y = Math.sin(((time*this.data.speed)/1000)*Math.PI);
         // console.log("time:" , time );
         // console.log("HHHHHHHH" + this.el.object3D.rotation.y);
       }  
