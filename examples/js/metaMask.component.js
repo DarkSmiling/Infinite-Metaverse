f3fe432678f3182ee457c4abad64e658336a1c5f
EnriqueVega1995 <vegacampos1995@gmail.com>
2022-01-26 13:42:32 -0600

    Added metamask login

--- examples/js/metaMask.component.js
+++ examples/js/metaMask.component.js
@@ -1,68 +1,54 @@
-AFRAME.registerComponent('panel-log-actions', {
+AFRAME.registerComponent('login', {
   init: function () {
     const el = this.el;
     const component = this;
 
-    window.addEventListener('keypress', async function (e) {
-      if (e.code !== 'KeyR') return;
-      if (!playerCollideWithClickable) return;
-      if (currentClickable !== el.id) return;
+    component.connectWallet();
 
-      component.connectWallet();
-    });
-
-    el.addEventListener('click', function () {
-      component.connectWallet();
-    })
+    // el.addEventListener('click', function () {
+    //   component.connectWallet();
+    // })
   },
   connectWallet: async function () {
     const userLogged = JSON.parse(window.localStorage.getItem('logged'));
     const el = this.el;
 
     if (!window.ethereum) {
-      this.el.setAttribute('show-panel-message', "message: Metamask is not installed.\n Please try again; panelWidth: 1.5; panelHeight: 0.6; position: 0 1.8 0.1; wrapCount: 30");
+      console.log("Metamask is not installed");
       setTimeout(function () {
-        el.removeAttribute('show-panel-message');
+        console.log("Metamask is not installed");
       }, 4000);
       return;
     }
 
     if (userLogged) {
-      this.el.setAttribute('show-panel-message', "message: You're already logged; panelWidth: 1.5; panelHeight: 0.6; position: 0 1.8 0.1; wrapCount: 22");
-      setTimeout(function () {
-        el.removeAttribute('show-panel-message');
-      }, 4000);
-      return;
+      console.log("Logged")
     }
-
-    this.el.removeAttribute('class');
-    player.object3D.parent.el.setAttribute('movement-controls', 'enabled: false');
-
     try {
       const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
       const account = accounts[0];
 
       if(account) {
-        this.el.setAttribute('show-panel-message', "message: Login successful; panelWidth: 1.5; panelHeight: 0.6; position: 0 1.8 0.1; wrapCount: 15");
+        console.log("Login successful");
         setTimeout(function () {
-          el.removeAttribute('show-panel-message');
+          console.log("Login successful");
         }, 4000);
         window.localStorage.setItem('logged', JSON.stringify(true));
         window.localStorage.setItem('account', JSON.stringify(account));
 
       } else {
-        this.el.setAttribute('show-panel-message', "message: There was a problem.\n Please try again.; panelWidth: 1.2; panelHeight: 0.4; position: 0 1.8 0.1; wrapCount: 20");
+        console.log("Problem to login");
         setTimeout(function () {
-          el.removeAttribute('show-panel-message');
+          console.log("Problem to login");
         }, 4000);
       }
     } catch (err) {
-      this.el.setAttribute('show-panel-message', "message: There was a problem.\n Please try again.; panelWidth: 1.5; panelHeight: 0.6; position: 0 1.8 0.1; wrapCount: 20");
-      setTimeout(function () {
-        el.removeAttribute('show-panel-message');
-      }, 4000);
+        console.log("Problem to login");
+        setTimeout(function () {
+          console.log("Problem to login");
+        }, 4000);
     }
-    this.el.setAttribute('class', 'clickable');
-    player.object3D.parent.el.setAttribute('movement-controls', 'enabled: true');
+    // this.el.setAttribute('class', 'clickable');
+    // player.object3D.parent.el.setAttribute('movement-controls', 'enabled: true');
   }
 })
