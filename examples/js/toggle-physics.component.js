cb51e1edd9b4b10d822fd2ce4d350aa2c0c0eb7a
EnriqueVega1995 <vegacampos1995@gmail.com>
2022-02-11 22:50:53 -0600

    Added new cube mpa

--- examples/js/toggle-physics.component.js
+++ examples/js/toggle-physics.component.js
@@ -13,7 +13,7 @@ AFRAME.registerComponent('toggle-physics', {
 
     NAF.utils.getNetworkedEntity(this.el).then((el) => {
       if (NAF.utils.isMine(el)) {
-        that.el.setAttribute('dynamic-body', 'shape:sphere');
+        that.el.setAttribute('dynamic-body', 'shape:sphere', 'mass: 0');
       } else {
         that.el.setAttribute('static-body', '');
       }
